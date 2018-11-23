ca73bcc78ddcec46f90030652188ebc20839fb4e
IvelinPenchev <ivelin.penchev.95@gmail.com>
2018-11-23 19:50:22 +0000

    Developed node to work as a simple program to convert force to pwm. Added a method to the class that checks if the force is realistic or not, bug fixes

--- include/catamaran_controller/force_to_pwm.h
+++ include/catamaran_controller/force_to_pwm.h
@@ -7,20 +7,25 @@
 
 class ForceToPWM
 { 
-    private:
+  private:
 
-    const float FUNCTION_LEFT_MOTOR_FORWARD_[3] = {-241.2, 833.3, 1669}; // the three parameters a,b,c of the quadratic function ax^2 + bx + c of the left motor conversion to PWM in forward motion
-    const float FUNCTION_LEFT_MOTOR_BACKWARD_[3] = {158.8, 712.6, 1345}; // the three parameters a,b,c of the quadratic function ax^2 + bx + c of the left motor conversion to PWM in backward motion
-    const float FUNCTION_RIGHT_MOTOR_FORWARD_[3] = {-172.1, 760.3, 1621}; // the three parameters a,b,c of the quadratic function ax^2 + bx + c of the right motor conversion to PWM in forward motion
-    const float FUNCTION_RIGHT_MOTOR_BACKWARD_[3] = {105.7, 641.8, 1357}; // the three parameters a,b,c of the quadratic function ax^2 + bx + c of the right motor conversion to PWM in backward motion
-    
-    public:
+  const float FUNCTION_LEFT_MOTOR_FORWARD_[3] = {-241.2, 833.3, 1669}; // the three parameters a,b,c of the quadratic function ax^2 + bx + c of the left motor conversion to PWM in forward motion
+  const float FUNCTION_LEFT_MOTOR_BACKWARD_[3] = {158.8, 712.6, 1345}; // the three parameters a,b,c of the quadratic function ax^2 + bx + c of the left motor conversion to PWM in backward motion
+  const float FUNCTION_RIGHT_MOTOR_FORWARD_[3] = {-172.1, 760.3, 1621}; // the three parameters a,b,c of the quadratic function ax^2 + bx + c of the right motor conversion to PWM in forward motion
+  const float FUNCTION_RIGHT_MOTOR_BACKWARD_[3] = {105.7, 641.8, 1357}; // the three parameters a,b,c of the quadratic function ax^2 + bx + c of the right motor conversion to PWM in backward motion
 
-    ForceToPWM();
-    ~ForceToPWM();
+  public:
 
-    float calculateRightPWM(float right_motor_force);
-    float calculateLeftPWM(float left_motor_force);
+  ForceToPWM();
+  ~ForceToPWM();
+
+  float left_motor_force_;
+  float right_motor_force_;
+  
+  bool isLeftForcePossible();
+  bool isRightForcePossible();
+  float calculateRightPWM();
+  float calculateLeftPWM();
     
 
 };
--- src/force_to_pwm.cpp
+++ src/force_to_pwm.cpp
@@ -4,31 +4,54 @@ ForceToPWM::ForceToPWM(){ }
 ForceToPWM::~ForceToPWM(){ }
 
 //Calculates the PWM signal based on the desired force
-float ForceToPWM::calculateLeftPWM(float left_motor_force)
+float ForceToPWM::calculateLeftPWM()
 {
   float pwm = 0.0;
   
-  if (left_motor_force == 0){
+  if (left_motor_force_ == 0){
     pwm = 0;
-  }else if(left_motor_force > 0){
-    pwm = FUNCTION_LEFT_MOTOR_FORWARD_[0]*left_motor_force*left_motor_force + FUNCTION_LEFT_MOTOR_FORWARD_[1]*left_motor_force + FUNCTION_LEFT_MOTOR_FORWARD_[2];
-  }else if(left_motor_force < 0){
-    pwm = FUNCTION_LEFT_MOTOR_BACKWARD_[0]*left_motor_force*left_motor_force + FUNCTION_LEFT_MOTOR_BACKWARD_[1]*left_motor_force + FUNCTION_LEFT_MOTOR_BACKWARD_[2];
+  }else if(left_motor_force_ > 0){
+    pwm = FUNCTION_LEFT_MOTOR_FORWARD_[0]*left_motor_force_*left_motor_force_ + FUNCTION_LEFT_MOTOR_FORWARD_[1]*left_motor_force_ + FUNCTION_LEFT_MOTOR_FORWARD_[2];
+  }else if(left_motor_force_ < 0){
+    pwm = FUNCTION_LEFT_MOTOR_BACKWARD_[0]*left_motor_force_*left_motor_force_ + FUNCTION_LEFT_MOTOR_BACKWARD_[1]*left_motor_force_ + FUNCTION_LEFT_MOTOR_BACKWARD_[2];
   }
   return pwm;
 }
 
 //Calculates the PWM signal based on the desired force
-float ForceToPWM::calculateRightPWM(float right_motor_force)
+float ForceToPWM::calculateRightPWM()
 {
   float pwm = 0.0;
   
-  if (right_motor_force == 0){
+  if (right_motor_force_ == 0){
     pwm = 0;
-  }else if(right_motor_force > 0){
-    pwm = FUNCTION_RIGHT_MOTOR_FORWARD_[0]*right_motor_force*right_motor_force + FUNCTION_RIGHT_MOTOR_FORWARD_[1]*right_motor_force + FUNCTION_RIGHT_MOTOR_FORWARD_[2];
-  }else if(right_motor_force < 0){
-    pwm = FUNCTION_RIGHT_MOTOR_BACKWARD_[0]*right_motor_force*right_motor_force + FUNCTION_RIGHT_MOTOR_BACKWARD_[1]*right_motor_force + FUNCTION_RIGHT_MOTOR_BACKWARD_[2];
+  }else if(right_motor_force_ > 0){
+    pwm = FUNCTION_RIGHT_MOTOR_FORWARD_[0]*right_motor_force_*right_motor_force_ + FUNCTION_RIGHT_MOTOR_FORWARD_[1]*right_motor_force_ + FUNCTION_RIGHT_MOTOR_FORWARD_[2];
+  }else if(right_motor_force_ < 0){
+    pwm = FUNCTION_RIGHT_MOTOR_BACKWARD_[0]*right_motor_force_*right_motor_force_ + FUNCTION_RIGHT_MOTOR_BACKWARD_[1]*right_motor_force_ + FUNCTION_RIGHT_MOTOR_BACKWARD_[2];
   }
   return pwm;
+}
+
+bool ForceToPWM::isLeftForcePossible()
+{
+if (left_motor_force_ < -1.47 || left_motor_force_ > 1.38)
+  {
+  std::cout<<"Force in the left motor is not realistic. ";
+  return false;
+  }
+  else
+    return true;
+}
+
+bool ForceToPWM::isRightForcePossible()
+{
+
+  if (right_motor_force_ < -1.48 || right_motor_force_ > 1.43)
+  {
+    std::cout<<"Force in the right motor is not realistic. ";
+    return false;
+  }
+  else
+    return true;
 }
