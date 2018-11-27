8079ba900db65ce3bd4004a6754c3e51cc6d2748
EduPonz <e.ponzs@gmail.com>
2018-11-27 17:51:36 +0000

    feed_forward lib included

--- src/feed_forward_reference.cpp
+++ src/feed_forward_reference.cpp
@@ -1,44 +1,45 @@
+#include <math.h>
 #include "catamaran_controller/feed_forward_reference.h"
 
-FeedForwardReference() 
+FeedForwardReference::FeedForwardReference() 
 {
   
 }
-~FeedForwardReference() {}
+FeedForwardReference::~FeedForwardReference() {}
 
 
-float FeedForwardReference::angleToGoal(float current_x, float current_y, float current_bearing)
-{
-  float angle;
-  angle = atan2((goal_position(1) - current_y), goal_position(0) - current_x) - current_bearing;
-  if (angle < -PI)    
-    angle = angle + 2*PI;
-  else if (angle > PI)
-    angle = angle - 2*PI;
-  return angle; 
-}
+// float FeedForwardReference::angleToGoal(float current_x, float current_y, float current_bearing)
+// {
+//   float angle;
+//   angle = atan2((goal_position(1) - current_y), goal_position(0) - current_x) - current_bearing;
+//   if (angle < -PI)    
+//     angle = angle + 2*PI;
+//   else if (angle > PI)
+//     angle = angle - 2*PI;
+//   return angle; 
+// }
 
-void FeedForwardReference::set_direction_of_turn()
-{
-  float angle = angleToGoal(initial_position(0), initial_position(1), initial_bearing_)
-  if (angle > 0)
-    direction_of_turn_ = true;
-  else
-    direction_of_turn_ = false;    
-}
+// void FeedForwardReference::set_direction_of_turn()
+// {
+//   float angle = angleToGoal(initial_position(0), initial_position(1), initial_bearing_)
+//   if (angle > 0)
+//     direction_of_turn_ = true;
+//   else
+//     direction_of_turn_ = false;    
+// }
 
-void FeedForwardReference::set_time_vector()
-{
-  vector<float> time(TOTAL_TIME*SAMPLES_PER_SECOND);
+// void FeedForwardReference::set_time_vector()
+// {
+//   vector<float> time(TOTAL_TIME*SAMPLES_PER_SECOND);
 
-  for (int i=0; i < TOTAL_TIME*SAMPLES_PER_SECOND; i++)
-  {
-    float j = float(i);
-    time[i] = j*TIME_INTERVAL;
-  }
+//   for (int i=0; i < TOTAL_TIME*SAMPLES_PER_SECOND; i++)
+//   {
+//     float j = float(i);
+//     time[i] = j*TIME_INTERVAL;
+//   }
 
-time_vector = time;
-}
+// time_vector = time;
+// }
 
 void FeedForwardReference::set_turning_torque()
 {
@@ -60,7 +61,7 @@ float FeedForwardReference::momental_linear_vel_turn (int iteration)
 
 float FeedForwardReference::momental_linear_pos_turn (int iteration)
 {
-  return (linear_pos_[iteration-1] + linear_vel_[iteration-1]*(TIME_INTERVAL) + linear_acc_[iteration-1]*(TIME_INTERVAL^2));
+  return (linear_pos_[iteration-1] + linear_vel_[iteration-1]*(TIME_INTERVAL) + linear_acc_[iteration-1] * pow(TIME_INTERVAL, 2));
 }
 
 float FeedForwardReference::momental_angular_acc_turn (int iteration)
@@ -75,6 +76,6 @@ float FeedForwardReference::momental_angular_vel_turn (int iteration)
 
 float FeedForwardReference::momental_angular_pos_turn (int iteration)
 {
- return (angular_pos_[iteration-1] + angular_vel_[iteration-1]TIME_INTERVAL + angular_acc_[iteration-1]*(TIME_INTERVAL^2);
+ return (angular_pos_[iteration-1] + angular_vel_[iteration-1] * TIME_INTERVAL + angular_acc_[iteration-1] * pow(TIME_INTERVAL, 2));
 }
 
